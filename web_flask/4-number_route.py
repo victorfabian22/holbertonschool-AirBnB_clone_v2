937cc0184dfc262eff8a7da6cb81f90645d9bacd
victorfabian22 <fabianromeroa22@gmail.com>
2023-10-16 21:45:31 -0700

    update

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def rout():
+    """Route '/'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route '/hbnb'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Adding an argument to the definition"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """Function that receives an argument"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Conditioning if it is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
